77aa2e6aeb1be6264ddbbd4e0e158749e96d0c14
Masa Sabobeh <masasabobeh03@gmail.com>
2025-10-02 00:52:22 +0300

    Task 9: port user.service.ts from course-tests

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -3,17 +3,29 @@ import {  User } from "./user.entity";
 import { createArgonHash } from "../auth/utils/argon.util";
 import { removeFields } from "../../shared/utils/object.utils";
 import { Role } from "../../shared/utils/types.utils";
+import { usersData } from "./user.data";
 
 
 
-class UserService{
-private repo=new UserRepository();
+export class UserService{
+    private readonly repo:UserRepository;
+    constructor(){
+        this.repo=new UserRepository(usersData);
+    }
+
 
 async  adminUserSeed(){
     const exist=this.repo.findByEmail('admin@no.com');
     if(!exist){
         const hashedPassword=await createArgonHash('admin123');
-        this.repo.create({name:'Admin' , password:hashedPassword , email:'admin@no.com' , role:'ADMIN'})
+        this.repo.create({
+            name:'Admin' , 
+            password:hashedPassword , 
+            email:'admin@no.com' , 
+            role:'ADMIN',
+            createdAt:new Date(),
+            updatedAt:new Date()
+        })
     }
 }
 getUsers(page:number , limit:number){
@@ -31,7 +43,14 @@ findUserByEmail(email:string): User|undefined{
     return this.repo.findByEmail(email);
 }
 createUser(name:string , email:string , password:string , role:Role = 'STUDENT'):Omit<User,'password'>{
-    const user=this.repo.create({name:name , email:email , password:password , role:role});
+    const user=this.repo.create({
+        name:name , 
+        email:email , 
+        password:password , 
+        role:role,
+        createdAt:new Date(),
+        updatedAt:new Date()
+    });
     
      const userWithoutPassword=removeFields(user , ['password']);
     return userWithoutPassword;
@@ -43,6 +62,8 @@ updateUser(id:string , name?:string , email?:string): User|null{
     
     if(name) payLoad.name=name;
     if(email) payLoad.email=email;
+    payLoad.updatedAt=new Date();
+
     
 
     return this.repo.update(id , payLoad);
@@ -55,4 +76,3 @@ return this.repo.delete(id);
 
 }
 
-export const userService=new UserService();
