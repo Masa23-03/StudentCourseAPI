92cfd3267e5283892c43ce6d027ba4b16b6ec4ea
Masa Sabobeh <masasabobeh03@gmail.com>
2025-10-02 01:32:13 +0300

    Task 9: port repo & route changes (app-only)

--- src/modules/courses/course.repository.ts
+++ src/modules/courses/course.repository.ts
@@ -4,6 +4,8 @@ import { Repository } from "../../shared/genericRepo";
 
 
 export class CourseRepository extends Repository<Course>{
-
+constructor(coursesArr: Course[]){
+    super(coursesArr);
+}
 
 }
