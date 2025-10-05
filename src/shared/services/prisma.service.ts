f25cd047ce29b8fd49866e66f0aceb3722cfb3b0
Masa Sabobeh <masasabobeh03@gmail.com>
2025-10-05 13:22:11 +0300

    prisma.service

--- src/shared/services/prisma.service.ts
+++ src/shared/services/prisma.service.ts
@@ -2,3 +2,6 @@ import { PrismaClient } from "../../generated/prisma";
 export const prisma = new PrismaClient({
   log: ["query", "info", "warn", "error"],
 });
+
+export const UserPrisma = prisma.user;
+export const CoursePrisma = prisma.course;
