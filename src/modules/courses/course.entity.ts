5482401a0754d10063aadf13a00964519eb2ebcb
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-02 09:41:09 +0200

    edited creator id in entity

--- src/modules/courses/course.entity.ts
+++ src/modules/courses/course.entity.ts
@@ -1,3 +1,6 @@
+import { ObjectId, SchemaDefinitionProperty } from "mongoose";
+import { User } from "../users/user.entity";
+
 export type Course = {
   id: string;
   title: string;
@@ -5,5 +8,5 @@ export type Course = {
   image: string | null; // MongoDB doesn't store undefined, so use null to keep the field explicitly empty
   createdAt: Date;
   updatedAt: Date;
-  creatorId: string;
+  creatorId: SchemaDefinitionProperty<ObjectId | User, Course>;
 };
