a1c44d39af03ccda3c363f14fe2745fdd69ea17d
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-02 09:07:08 +0200

    editedCourseEntity

--- src/modules/courses/course.entity.ts
+++ src/modules/courses/course.entity.ts
@@ -1,10 +1,9 @@
-
-export interface Course{
+export interface Course {
   id: string;
   title: string;
   description: string;
-  image?: string|undefined; 
+  image: string | null; // MongoDB doesn't store undefined, so use null to keep the field explicitly empty
   createdAt: Date;
   updatedAt: Date;
-  creatorId:string;
+  creatorId: string;
 }
