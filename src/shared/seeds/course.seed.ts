0dc63214531e47e46549ada4e83aa29962b5d40b
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-03 00:48:25 +0200

    seed function

--- src/shared/seeds/course.seed.ts
+++ src/shared/seeds/course.seed.ts
@@ -1,30 +1,16 @@
 import { faker } from "@faker-js/faker";
 import { Course } from "../../modules/courses/course.entity";
 import { COURSE_DATASET } from "../data/course.dataset";
-import { usersData } from "../../modules/users/user.data";
-import { CustomError } from "../utils/error.utils";
-import { User } from "../../modules/users/user.entity";
 
-function pickUserIdByRole(role: "ADMIN" | "COACH" = "ADMIN"): string {
-  const u = usersData.find((x: User) => x.role === role);
-  if (!u) {
-    throw new Error(`No seeded user with role ${role}`);
-  }
-  return u.id;
-}
-
-export const createRandomCourse = (
-  role: "ADMIN" | "COACH" = "ADMIN"
-): Course => {
+export const createRandomCourse = (role: "ADMIN" | "COACH" = "ADMIN") => {
   const courseElement = faker.helpers.arrayElement(COURSE_DATASET);
 
-  const randomCourse: Course = {
-    id: faker.string.uuid(),
+  const randomCourse: Omit<
+    Course,
+    "authorId" | "id" | "updatedAt" | "createdAt" | "image" | "creatorId"
+  > = {
     title: courseElement.name,
     description: courseElement.description,
-    createdAt: faker.date.past(),
-    updatedAt: faker.date.future(),
-    creatorId: pickUserIdByRole(role),
   };
   return randomCourse;
 };
--- src/shared/seeds/user.seed.ts
+++ src/shared/seeds/user.seed.ts
@@ -2,37 +2,29 @@ import { faker } from "@faker-js/faker";
 import { User } from "../../modules/users/user.entity";
 import { Role } from "../utils/types.utils";
 
-function makeRandomUser(role:User['role'] , overrides: Partial<User>={}):User{
-    const randomUser:User={
-        id:faker.string.uuid(),
-        name:faker.person.fullName(),
-        role:role,
-        password:faker.internet.password({length:8}),
-        email:faker.internet.email().toLowerCase(),
-        createdAt:faker.date.past(),
-        updatedAt:faker.date.future(),
+function makeRandomUser(role: User["role"], overrides: Partial<User> = {}) {
+  const randomUser: Omit<User, "id" | "createdAt" | "updatedAt"> = {
+    name: faker.person.fullName(),
+    role: role,
+    password: faker.internet.password({ length: 8 }),
+    email: faker.internet.email().toLowerCase(),
+  };
 
-    };
-
-    return {...randomUser , ...overrides};
-
+  return { ...randomUser, ...overrides };
 }
-const emailFormat=(role:Role , i=0)=>
- i===0? `${role.toLowerCase()}@no.com` : `${role.toLowerCase()}+${i}@no.com`
+const emailFormat = (role: Role, i = 0) =>
+  i === 0
+    ? `${role.toLowerCase()}@no.com`
+    : `${role.toLowerCase()}+${i}@no.com`;
 
-
-export function createRandomStudent():User{
-return makeRandomUser('STUDENT');
-
-
+export function createRandomStudent() {
+  return makeRandomUser("STUDENT");
 }
 
-export function createAdminUser(i=0):User{
-return makeRandomUser('ADMIN' , {email:emailFormat('ADMIN' , i)});
-
+export function createAdminUser(i = 0) {
+  return makeRandomUser("ADMIN", { email: emailFormat("ADMIN", i) });
 }
 
-export function createCoachUser(i=0):User{
-    return makeRandomUser('COACH' , {email:emailFormat('COACH' , i)});
-    
+export function createCoachUser(i = 0) {
+  return makeRandomUser("COACH", { email: emailFormat("COACH", i) });
 }
