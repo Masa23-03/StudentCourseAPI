12716802dcbfef6a96756a5883c6f19ae01c6c08
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-02 08:59:29 +0200

    addMongooseConnectionAndImplement-toJSONFieldCleanup(_id, __v, password)

--- src/shared/utils/declarationMerging.utils.ts
+++ src/shared/utils/declarationMerging.utils.ts
@@ -1,31 +1,31 @@
-//? declaration merging.ts 
+//? declaration merging.ts
 import { UnifiedErrorApiResponse } from "../middlewares/response.middleware";
 import { User } from "../../modules/users/user.entity";
 
-export type MyEnvs={
-PORT:number,
-JWT_SECRET:string,
-MAX_IMAGE_SIZE_MB:number,
-NODE_ENV:"development"|"staging"|"production"|"test",
+export type MyEnvs = {
+  PORT: number;
+  JWT_SECRET: string;
+  MAX_IMAGE_SIZE_MB: number;
+  NODE_ENV: "development" | "staging" | "production" | "test";
+  MONGODB_URL: string;
+};
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends MyEnvs {}
+  }
 }
 
-declare global{ 
-    namespace NodeJS{
-    interface ProcessEnv extends MyEnvs{}
-}
-}
-
-declare global{
-    namespace Express{
-        interface Response{
-            create:(data:object)=>this;
-            ok:(data:object)=>this;
-            error:(error:UnifiedErrorApiResponse)=>this;
-
-        }
-
-        interface Request{
-            user?:Pick<User , 'id'|'name'|'role'>
-        }
+declare global {
+  namespace Express {
+    interface Response {
+      create: (data: object) => this;
+      ok: (data: object) => this;
+      error: (error: UnifiedErrorApiResponse) => this;
     }
+
+    interface Request {
+      user?: Pick<User, "id" | "name" | "role">;
+    }
+  }
 }
