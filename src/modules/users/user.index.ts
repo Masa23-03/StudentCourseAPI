fd635e46ba7c076288f4c157d3ab2c84950d2658
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-02 16:53:06 +0200

    added user repo singleton

--- src/modules/users/user.index.ts
+++ src/modules/users/user.index.ts
@@ -1,7 +1,9 @@
 import { UserController } from "./user.controller";
+import { UserRepository } from "./user.repository";
 import { UserService } from "./user.service";
 
-export const userService=new UserService();
+export const userService = new UserService();
 
-export const userController=new UserController(userService);
+export const userController = new UserController(userService);
 
+export const userRepo = new UserRepository();
