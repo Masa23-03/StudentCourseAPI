1e400911cae57ac7200dd072c173b5bbc38842d5
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-02 17:06:21 +0200

    course service

--- src/modules/courses/course.service.ts
+++ src/modules/courses/course.service.ts
@@ -1,82 +1,57 @@
-import { CourseRepository } from "./course.repository"
-import { Course } from "./course.entity"
+import { CourseRepository } from "./course.repository";
+import { Course } from "./course.entity";
 import { removeFields } from "../../shared/utils/object.utils";
 import { coursesData } from "./course.data";
 import { userService } from "../users/user.index";
 import { courseResponse } from "./types/dto.types";
+import { courseRepo } from "./course.index";
+import { CreatePayload } from "../../shared/utils/types.utils";
 
+export class CourseService {
+  getCourses(page: number = 1, limit: number = 10) {
+    return courseRepo.findAll(page, limit);
+  }
+  getCourse(id: string) {
+    return courseRepo.findById(id);
+  }
 
-export class CourseService{
-    
-private readonly repo;
-constructor(){
-    this.repo=new CourseRepository(coursesData);
+  async createCourse(
+    title: string,
+    description: string,
+    creatorId: string,
+    image?: string
+  ) {
+    //check id user exist
+    const creator = await userService.getUser(creatorId);
+    if (!creator) return null;
+    //check user role ==> only admin and coach can create course
+    if (creator.role === "STUDENT") return null;
 
+    return courseRepo.create({
+      title: title,
+      description: description,
+      creatorId: creatorId,
+      image: image ?? null,
+    });
+  }
+  updateCourse(
+    courseId: string,
+    title?: string,
+    description?: string,
+    image?: string
+  ) {
+    const course = courseRepo.findById(courseId);
+    if (!course) return null;
+
+    const updatedCourse: Partial<Course> = {
+      updatedAt: new Date(),
+    };
+    if (title) updatedCourse.title = title;
+    if (description) updatedCourse.description = description;
+    if (image) updatedCourse.image = image;
+    return courseRepo.update(courseId, updatedCourse);
+  }
+  deleteCourse(id: string) {
+    return courseRepo.delete(id);
+  }
 }
-getCourses(){
-    return this.repo.findAll();
-}
-getCourse(id:string): courseResponse|null {
-    const course=this.repo.findById(id);
-    if(!course)return null;
-    const courseWithoutUserId=removeFields(course , ['creatorId']);
-    return courseWithoutUserId;
- 
-}
-
-
-createCourse(title:string , description:string ,creatorId:string , image?:string  ):courseResponse | null{
-//check id user exist
-const creator=userService.getUser(creatorId);
-if(!creator)return null;
-//check user role ==> only admin and coach can create course
-if (creator.role==='STUDENT') return null;
-
-
-const course:Omit<Course, 'id' > ={
-
-title , 
-description , 
-creatorId ,
-createdAt:new Date(),
-updatedAt:new Date(),
-
-}
-if(image) course.image=image;
-
-const courseCreated= this.repo.create(course);
-const courseWithoutUserId=removeFields(courseCreated , ['creatorId']);
-return courseWithoutUserId;
-
-}
-updateCourse( courseId:string ,  title?:string , description?:string , image?:string ):courseResponse | null{
-
-const course = this.repo.findById(courseId); 
-if(!course)return null;
-
-
-
-    const updatedCourse:Partial<Course> = {
-        updatedAt:new Date()
-    }
-    if(title)updatedCourse.title=title;
-    if(description)updatedCourse.description=description;
-    if(image)updatedCourse.image=image;
-    
-    
-    const updateCourse= this.repo.update(courseId , updatedCourse);
-    if(!updateCourse)return null;
-    return removeFields(updateCourse, ['creatorId']);
-    return null;
-
-
-}
-deleteCourse(id:string):boolean{
-
-    return this.repo.delete(id);
-    
-}
-
-}
-
-
