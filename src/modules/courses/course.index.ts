cbc1207ec83f017cc47d84e627497d291656786e
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-02 16:54:57 +0200

    added course repo singleton

--- src/modules/courses/course.index.ts
+++ src/modules/courses/course.index.ts
@@ -1,5 +1,7 @@
 import { CourseController } from "./course.controller";
+import { CourseRepository } from "./course.repository";
 import { CourseService } from "./course.service";
 
-export const courseService=new CourseService();
-export const courseController=new CourseController(courseService);
+export const courseService = new CourseService();
+export const courseController = new CourseController(courseService);
+export const courseRepo = new CourseRepository();
