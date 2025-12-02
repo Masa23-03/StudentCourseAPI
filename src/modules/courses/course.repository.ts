38a74788c2431cbcdca401d65a7c811451590781
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-02 14:36:13 +0200

    added Model in super

--- src/modules/courses/course.repository.ts
+++ src/modules/courses/course.repository.ts
@@ -1,4 +1,9 @@
 import { MongooseRepository } from "../../shared/genericRepo";
 import { Course } from "./course.entity";
+import { CourseModel } from "./course.model";
 
-export class CourseRepository extends MongooseRepository<Course> {}
+export class CourseRepository extends MongooseRepository<Course> {
+  constructor() {
+    super(CourseModel);
+  }
+}
--- src/modules/users/user.repository.ts
+++ src/modules/users/user.repository.ts
@@ -1,7 +1,11 @@
 import { MongooseRepository } from "../../shared/genericRepo";
 import { User } from "./user.entity";
+import { UserModel } from "./user.model";
 
 export class UserRepository extends MongooseRepository<User> {
+  constructor() {
+    super(UserModel);
+  }
   async findByEmail(email: string): Promise<User | null> {
     const record = await this.model.findOne({ email }).exec();
     return record as User | null;
