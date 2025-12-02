58fe44cbe781b7710eacc0ea0661239c3b715b42
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-02 13:57:32 +0200

    generic Repo is ready

--- src/shared/genericRepo.ts
+++ src/shared/genericRepo.ts
@@ -1,6 +1,5 @@
 import { Model } from "mongoose";
 import { GenericRepositoryI } from "./genericRepo.Interface";
-import { MongodbModelsType } from "./utils/constants.utils";
 import { CreatePayload, UpdatePayload } from "./utils/types.utils";
 
 export class MongooseRepository<T> implements GenericRepositoryI<T> {
@@ -10,22 +9,31 @@ export class MongooseRepository<T> implements GenericRepositoryI<T> {
     page: number,
     limit: number
   ): Promise<{ records: T[]; totalRecords: number }> {
-    const items = await this.model
+    const records = (await this.model
       .find()
       .skip((page - 1) * limit)
       .limit(limit)
-      .exec();
+      .lean()
+      .exec()) as T[];
     const totalRecords = await this.model.countDocuments().exec();
-    return { records: items, totalRecords };
+    return { records: records, totalRecords };
   }
   async findById(id: string): Promise<T | null> {
-    return this.model.findById(id).exec();
+    const doc = await this.model.findById(id).exec();
+    return doc as T | null;
   }
   async create(payload: CreatePayload<T>): Promise<T> {
-    return await this.model.create(payload);
+    const record = await this.model.create(payload as any);
+    const toJson = (record as any).toJSON ? (record as any).toJSON() : record;
+    return toJson;
   }
   async update(id: string, payLoad: UpdatePayload<T>): Promise<T | null> {
-    return this.model.findByIdAndUpdate(id, payLoad, { new: true }).exec();
+    const record = await this.model
+      .findByIdAndUpdate(id, payLoad as any, {
+        new: true,
+      })
+      .exec();
+    return record as T | null;
   }
   async delete(id: string): Promise<boolean> {
     const result = await this.model.findByIdAndDelete(id).exec();
