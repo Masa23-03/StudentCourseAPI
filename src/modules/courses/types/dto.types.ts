1bc7be33ba6364aadd17f636f4beac2f6027ccdf
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-03 10:18:03 +0200

    image become optional in dto

--- src/modules/courses/types/dto.types.ts
+++ src/modules/courses/types/dto.types.ts
@@ -1,5 +1,7 @@
 import { Course } from "../course.entity";
 
-export type courseResponse= Omit<Course , 'creatorId'>;
-export type createOrUpdateCourse=Pick<Course , 'title'|'description'|'image'>
-
+export type courseResponse = Omit<Course, "creatorId">;
+export type createOrUpdateCourse = Omit<
+  Course,
+  "id" | "createdAt" | "updatedAt" | "creatorId"
+> & { image?: Course["image"] };
