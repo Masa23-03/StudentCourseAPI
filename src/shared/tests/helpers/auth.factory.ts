831c2b32ba458f62bcbf5393fda05c110b15af7b
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-03 08:24:08 +0200

    added tests from the testing task

---
+++ src/shared/tests/helpers/auth.factory.ts
@@ -0,0 +1,29 @@
+import { usersData } from "../../../modules/users/user.data";
+import { User } from "../../../modules/users/user.entity";
+import { CustomError } from "../../utils/error.utils";
+import { HttpErrorStatus, Role } from "../../utils/types.utils";
+import { signJwt } from "../../../modules/auth/utils/jwt.util";
+
+export function getSeededUserByRole(role: Role, index = 0) {
+  const seededUsers = usersData.filter((user) => user.role === role);
+  const user = seededUsers[index];
+  if (!user)
+    throw new CustomError(
+      `seeded user with ${role} not fount`,
+      "USER",
+      HttpErrorStatus.NotFound
+    );
+
+  return user;
+}
+
+export function makeTokenForSeededUser(role: Role, index = 0): string {
+  const seededUser = getSeededUserByRole(role, index);
+  const token = signJwt({
+    sub: seededUser.id!,
+    name: seededUser.name,
+
+    role: seededUser.role,
+  });
+  return token;
+}
